a860bd3658faddf9d04df3b1b094ce71690b2f1b
Monika <molinskamonika+github@gmail.com>
2020-01-06 15:27:39 +0100

    setup basic routing

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => (
+  <div>
+    <h2>Four oh four... Sorry, no page found.</h2>
+  </div>
+)
+
+export default NotFound
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/App'
-import StorePicker from './components/StorePicker'
+import Router from './components/Router'
 import './css/style.css'
 
 render(
-  // <StorePicker />,
-  <App />,
+  <Router />,
   document.querySelector('#main')
 )
